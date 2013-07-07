d827cc85502052010e21178f9b86d5833d538c8c
Morteza Shahriari Nia <info@>
2013-07-07 13:32:59 -0400

    Adding model, generating table. Found mass assignment security hole and attr_accssible in model for rails 3.0 but for rails 4.0 we need to use strong_parameters in controller. Will catch upon that later on as it's a bit obscure for now. Issue #1

--- ruby_on_rails/blog/app/controllers/posts_controller.rb
+++ ruby_on_rails/blog/app/controllers/posts_controller.rb
@@ -5,7 +5,14 @@ class PostsController < ApplicationController
   # http://guides.rubyonrails.org/routing.html
   # This will be the default page when someone access the posts URL
   # just by defining it even empty we have a posts index page -> we need a view file for it to display
+  
+  # Regarding mass_assignment 
+  # http://blog.remarkablelabs.com/2012/12/strong-parameters-rails-4-countdown-to-2013
+  # http://carnal0wnage.attackresearch.com/2013/05/rails-strong-parameters.html
+  
   def index    
   
   end
+  
+  
 end
