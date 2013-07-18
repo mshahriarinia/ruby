9ff2b86247e06e6f6f906e67c33ea5892933ddd9
Morteza Shahriari Nia <info@>
2013-07-18 00:47:49 -0400

    First test case.

--- railstutorial.org/sample_app/spec/requests/static_pages_spec.rb
+++ railstutorial.org/sample_app/spec/requests/static_pages_spec.rb
@@ -1,11 +1,22 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-  describe "GET /static_pages" do
-    it "works! (now write some real specs)" do
+#  describe "GET /static_pages" do
+#    it "works! (now write some real specs)" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get static_pages_index_path
-      response.status.should be(200)
+#      get static_pages_index_path
+#      response.status.should be(200)
+#    end
+#  end
+
+  describe "Home page" do  # The first line indicates that we are describing the Home page.
+    # This description is just a string and is not important.
+
+    it "should have the content 'Sample App'" do    # describing string
+      visit '/static_pages/home'                    #  simulate visiting the URL /static_pages/home in a browser
+      expect(page).to have_content('Sample App')  # when you visit the Home page at /static_pages/home, the content should contain the words “Sample App”
     end
   end
 end
+
+# What goes inside the quote marks is irrelevant to RSpec, and is intended to be descriptive to human readers.
