c4c975f50734c08d0391230f69724760f1c46095
Morteza Shahriari Nia <m.shahriarinia@gmali.com>
2013-07-18 23:09:35 -0400

    Dynamic title test cases.

--- railstutorial.org/sample_app/spec/requests/static_pages_spec.rb
+++ railstutorial.org/sample_app/spec/requests/static_pages_spec.rb
@@ -16,6 +16,11 @@ describe "StaticPages" do
       visit '/static_pages/home'                    #  simulate visiting the URL /static_pages/home in a browser
       expect(page).to have_content('Sample App')  # when you visit the Home page at /static_pages/home, the content should contain the words “Sample App”
     end
+
+    it "should have the right title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+    end
   end
 
   describe "Help page" do
@@ -24,6 +29,11 @@ describe "StaticPages" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+    end
   end
 
   describe "About page" do
@@ -31,6 +41,11 @@ describe "StaticPages" do
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
+    end
+
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
     end
   end
 end
